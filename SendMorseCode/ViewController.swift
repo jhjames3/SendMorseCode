210124c3ef966d15ae66c8fb7c4e3adfd999ac66
John James <jjames3@me.com>
2019-03-22 10:24:11 -0400

    Initial Commit

---
+++ SendMorseCode/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SendMorseCode
+//
+//  Created by John James on 3/22/19.
+//  Copyright © 2019 John James. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
